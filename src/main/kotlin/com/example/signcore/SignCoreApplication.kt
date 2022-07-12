1e9ec6065d58ae53f85654e09ba9904614c0ee07
lockon2200 <H4zypeqsr-J5mXZr9m47>
2022-07-12 15:50:42 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/signcore/SignCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.signcore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SignCoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<SignCoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/signcore/SignCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.signcore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SignCoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
